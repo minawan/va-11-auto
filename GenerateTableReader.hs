7f961ade12671d34300774960e54710d9e68d6db
Minaki Seirawan <Seirawan.Minaki@gmail.com>
2019-12-31 14:51:59 -0800

    Read ScreenElement table as CSV

--- GenerateTableReader.hs
+++ GenerateTableReader.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
+import qualified Data.List as List
 import qualified Data.Text as Text
 import Data.Text (Text)
 import Data.Text.IO as I hiding (putStrLn)
@@ -29,8 +31,55 @@ instance FromJSON Column
 getRawJSON :: FilePath -> IO B.ByteString
 getRawJSON = B.readFile
 
+convertColumnsToHaskell :: [Column] -> Text
+convertColumnsToHaskell columns =
+    Text.concat . List.intersperse separator $ map declare columns
+  where
+    separator = "\n  , "
+    declare (Column columnName columnType) =
+      Text.concat [columnName, " :: !", columnType]
+
+-- Convert varName and a list [(columnName = name_i, columnType = _)] to:
+--   varName .: "name_1" [<*> varName .: "name_i"]*
+getParseNamedRecordInApplicativeForm :: [Column] -> Text -> Text
+getParseNamedRecordInApplicativeForm columns varName =
+    Text.concat . List.intersperse separator $ map project names
+  where
+    separator = " <*> "
+    project component = Text.concat [ varName, " .: \"", component, "\""]
+    names = map columnName columns
+
+convertToHaskell :: Table -> Text
+convertToHaskell (Table tableName tableColumns) =
+    Text.unlines
+      [ "{-# LANGUAGE OverloadedStrings #-}"
+      , ""
+      , "import Data.Csv"
+      , "import qualified Data.ByteString.Lazy as B"
+      , "import Data.Vector (Vector)"
+      , ""
+      , Text.concat ["data ", tableName, " = ", tableName]
+      , Text.concat ["  { ", convertColumnsToHaskell tableColumns ]
+      , "  } deriving (Show)"
+      , ""
+      , Text.concat ["instance FromNamedRecord ", tableName, " where"]
+      , Text.concat [ "  parseNamedRecord record = ", tableName, " <$> "
+                    , getParseNamedRecordInApplicativeForm tableColumns "record"
+                    ]
+      , ""
+      , "main :: IO ()"
+      , "main = do"
+      , "  csvData <- B.readFile \"ScreenElement.csv\""
+      , Text.concat [ "  let decodedCsvData = decodeByName csvData"
+                    , " :: Either String (Header, Vector ", tableName, ")"
+                    ]
+      , "  case decodedCsvData of"
+      , "    Left err -> putStrLn err"
+      , "    Right (_, val) -> putStrLn $ show val"
+      ]
+
 main = do
   res <- (eitherDecode <$> getRawJSON jsonFile) :: IO (Either String Table)
   case res of
     Left err -> putStrLn err
-    Right table -> I.writeFile outputFile . Text.pack $ show table
+    Right table -> I.writeFile outputFile $ convertToHaskell table
