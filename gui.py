19d8d55aacde19993e7378d3736baa8008e09a61
Minaki Seirawan <seirawan.minaki@gmail.com>
2022-09-28 18:21:42 -0700

    Encapsulate option menus

--- gui.py
+++ gui.py
@@ -2,50 +2,56 @@ import csv
 import tkinter as tk
 import os
 
-label_texts = ['Drink Name', 'Reset', 'Slot', 'Double', 'Add Opt.', 'Serve']
-boolean_selection = ['Y', 'N']
-drink_selection = []
+label_width = 10
+label_height = 2
+boolean_options = ['Y', 'N']
+drink_options = []
 with open('DrinkRecipe.csv', newline='') as csvfile:
   reader = csv.reader(csvfile)
   for row in reader:
-    drink_selection.append(row[0])
-drink_selection.remove('name')
-slot_selection = ['LEFT_SLOT', 'RIGHT_SLOT']
-label_width = 10
-label_height = 2
+    drink_options.append(row[0])
+drink_options.remove('name')
+slot_options = ['LEFT_SLOT', 'RIGHT_SLOT']
 
-def initOptionMenu(selection, window, row):
-  var = tk.StringVar()
-  var.set(selection[0])
-  dropdown = tk.OptionMenu(window, var, *selection)
-  dropdown.grid(row=row, column=1)
-  return var
+class OptionMenu():
+  def __init__(self, parent, label, options):
+    frame = tk.Frame(parent)
+    label = tk.Label(frame, text=label, width=label_width, height=label_height)
+    label.pack(side=tk.LEFT)
+    var = tk.StringVar()
+    var.set(options[0])
+    self.var = var
+    dropdown = tk.OptionMenu(frame, var, *options)
+    dropdown.pack(side=tk.RIGHT)
+    frame.pack(side=tk.TOP)
+
+  def value(self):
+    return self.var.get()
 
 class App():
   def __init__(self):
-    self.window = tk.Tk()
-    for text in label_texts:
-      label = tk.Label(text=text, width=label_width, height=label_height)
-      label.grid()
-    self.drink_name = initOptionMenu(drink_selection, self.window, 0)
-    self.reset = initOptionMenu(boolean_selection, self.window, 1)
-    self.slot = initOptionMenu(slot_selection, self.window, 2)
-    self.double = initOptionMenu(boolean_selection, self.window, 3)
-    self.add_opt = initOptionMenu(boolean_selection, self.window, 4)
-    self.serve = initOptionMenu(boolean_selection, self.window, 5)
-    run_button = tk.Button(text='Run')
-    run_button.grid()
+    window = tk.Tk()
+    window.title('va-11-auto')
+    self.drink_name = OptionMenu(window, 'Drink Name', drink_options)
+    self.reset = OptionMenu(window, 'Reset', boolean_options)
+    self.slot = OptionMenu(window, 'Slot', slot_options)
+    self.double = OptionMenu(window, 'Double', boolean_options)
+    self.add_opt = OptionMenu(window, 'Add Opt.', boolean_options)
+    self.serve = OptionMenu(window, 'Serve', boolean_options)
+    run_button = tk.Button(window, text='Run')
+    run_button.pack(side=tk.TOP)
     run_button.bind('<Button-1>', self.handle_click)
+    self.window = window
 
   def handle_click(self, event):
     with open('Input.csv', 'w') as config:
       config.write('drink_name,reset,slot,double,add_opt,serve\n')
-      config.write(','.join([self.drink_name.get(), self.reset.get(), self.slot.get(), self.double.get(), self.add_opt.get(), self.serve.get()]) + '\n')
+      config.write(','.join([self.drink_name.value(), self.reset.value(), self.slot.value(), self.double.value(), self.add_opt.value(), self.serve.value()]) + '\n')
     os.system('./run.sh')
 
-  def mainloop(self):
+  def run(self):
     self.window.mainloop()
 
 if __name__ == "__main__":
   app = App()
-  app.mainloop()
+  app.run()
